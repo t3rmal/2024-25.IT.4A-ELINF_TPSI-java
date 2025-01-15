dfd03ac6038b9b8adc054f32af92bb237bdd9b05
t3rmal <cristiangrimaldi305@gmail.com>
2025-01-15 09:45:46 +0000

    versione Mthread

--- src/PasswordBreaker.java
+++ src/PasswordBreaker.java
@@ -5,16 +5,19 @@ public class PasswordBreaker {
         // viene creato un file protetto da una password segreta ...
         PasswordProtectedFile file = new PasswordProtectedFile(PASSWORD_LENGTH);
         // e un cracker che deve scoprire la password del file
-        PasswordCracker cracker = new PasswordCracker(file, PASSWORD_LENGTH);
-
+        PasswordCracker cracker = new PasswordCracker(file, PASSWORD_LENGTH, 1);
+        PasswordCracker cracker2 = new PasswordCracker(file, PASSWORD_LENGTH, 2);
+        PasswordCracker cracker3 = new PasswordCracker(file, PASSWORD_LENGTH, 3);
         System.out.println("Un file password protected e' stato creato. ");
         System.out.println("Stiamo cercando di violare la password... ");
         System.out.println();
-        Cronometro crono = new Cronometro();
-        crono.start();
-        String secret_password = cracker.crack_it();
-        crono.stop();
-        System.out.println("La password segreta: " + secret_password);
-        System.out.println("e' stata violata in: " + crono.elapsed() + " millisecondi");
+        //Cronometro crono = new Cronometro();
+       // crono.start();
+        cracker.start();
+        cracker3.start();
+        cracker2.start();
+        //crono.stop();
+       //System.out.println("La password segreta: " + secret_password);
+        //System.out.println("e' stata violata in: " + crono.elapsed() + " millisecondi");
     }
 }
--- src/PasswordCracker.java
+++ src/PasswordCracker.java
@@ -1,18 +1,20 @@
 // Un oggetto di questa classe rappresenta un hacker che cerca di violare
 // la password segreta per aprire il file protetto che viene ricevuto nel
 // costruttore.
-public class PasswordCracker {
+public class PasswordCracker extends Thread {
     private PasswordGenerator passwordGenerator;
     private PasswordProtectedFile fileToCrack;
     private int password_length;
     private String password;
+    private int num;
 
-    public PasswordCracker(PasswordProtectedFile file, int password_length) {
+    public PasswordCracker(PasswordProtectedFile file, int password_length, int num) {
         this.password_length = password_length;
         this.passwordGenerator = new PasswordGenerator();
         this.fileToCrack = file;
+        this.num = num;
     }
-    public String crack_it() {
+    private String crack_it() {
         while(fileToCrack.open(password)==false){
             password = passwordGenerator.generate(password_length);
         }
@@ -22,4 +24,14 @@ public class PasswordCracker {
         return password;
     }
 
+
+    public void run(){
+        Cronometro crono = new Cronometro();
+        crono.start();
+        String secret_password=crack_it();
+        crono.stop();
+        System.out.println("e' stata violata in " + this.num + " :" + crono.elapsed() + " millisecondi");
+        System.out.println("La password segreta: " + secret_password);
+
+    }
 }
